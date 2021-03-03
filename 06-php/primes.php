3785ee1aca9089c9bc739fd1e55dc78f8c5ea628
Josh Schoep <josh@woodridgesoftware.com>
2021-03-03 12:25:01 -0700

    Finished php part 1: prime numbers

---
+++ 06-php/primes.php
@@ -0,0 +1,39 @@
+<?php
+//from geeksforgeeks
+function primeCheck($num) {
+    if($num == 1) {
+        return 0;
+    }
+    for($i=2; $i <= $num / 2; $i++) {
+        if($num % $i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+?>
+
+<head>
+    <title>PHP Prime Counter</title>
+</head>
+<body>
+<?php   
+    $n = 1000;
+    if(isset($_GET['n'])){
+        $n = $_GET['n'];
+    }
+
+    if($n < 0 || !ctype_digit($n)){
+        exit("Requested max value ".$n." is not an integer, or is below 0");
+    }
+
+    echo "<h1>The first ".$n." primes:</h1>";
+    echo "<p>";
+    for($i = 2; $i < $n; $i++){
+        if(primeCheck($i)){
+            echo $i.", ";
+        }
+    }
+    echo "</p>";
+
+?>
