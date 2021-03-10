c22f8e9c069d5643a9c38d72b56b2afd1dc9c499
Josh Schoep <josh@woodridgesoftware.com>
2021-03-10 14:31:56 -0700

    Changed union to all since overlap can happen

--- 07-mysql/get_lonely_creatures.sql
+++ 07-mysql/get_lonely_creatures.sql
@@ -5,7 +5,7 @@ USE joshuaschoep_test_db;
 SELECT id, CONCAT(first_name, ' ', last_name) AS name
     FROM People
     WHERE (SELECT COUNT(*) FROM Pets WHERE People.id = Pets.owner_id) = 0
-UNION DISTINCT
+UNION ALL
 SELECT id, name 
     FROM Pets
     WHERE (SELECT COUNT(*) FROM People WHERE Pets.owner_id = People.id) = 0
